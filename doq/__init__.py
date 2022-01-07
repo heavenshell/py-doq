01457e4365eb24896e47b1656e58e656b4f6a5a1
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2022-01-08 00:08:16 +0900

    Bump version 0.9.1

--- doq/__init__.py
+++ doq/__init__.py
@@ -6,4 +6,4 @@ from doq.config import find_config # noqa F401
 from doq.parser import parse   # noqa F401
 from doq.template import Template  # noqa F401
 
-__version__ = '0.9.0'
+__version__ = '0.9.1'
