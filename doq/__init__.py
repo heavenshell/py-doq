fb29eb210b0ab5893d2ecd37ae2a6398b597e97d
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2023-08-01 01:13:20 +0900

    Fix stop use setuptools_scm for to manage versions manually
    Workflow
    - Create bump version Pull request
    - Merge to main branch
    - Add tag
    - Create GitHub release by release-drafter
    - Publish to PyPi

--- doq/__init__.py
+++ doq/__init__.py
@@ -6,8 +6,4 @@ from doq.config import find_config # noqa F401
 from doq.parser import parse   # noqa F401
 from doq.template import Template  # noqa F401
 
-# avoid bug when `pyproject-build`
-try:
-    from ._version import __version__
-except ImportError:
-    __version__ = ""
+__version__ = '0.9.1'
